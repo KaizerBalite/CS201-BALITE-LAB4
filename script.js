ed552cfcb64eab4fd61657ac9bd5280d77c30f54
Kaizer Sallong Balite <146916590+KaizerBalite@users.noreply.github.com>
2023-10-09 19:14:36 -0700

    feat: Activity 4 in DSA
    Switch Conditional Statement

---
+++ script.js
@@ -0,0 +1,17 @@
+//Activity 4
+
+let fruit = 'banana';
+
+switch(fruit){
+    case 'banana':
+        console.log('Banana is good!');
+        break;
+    case 'orange':
+        console.log('I am not a fan of Orange.');
+        break;
+    case 'apple':
+        console.log('How you like them Apples?');
+        break;
+    default:
+        console.log('I have never heard of that fruit.');
+}
